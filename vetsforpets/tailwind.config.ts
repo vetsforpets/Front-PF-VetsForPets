2fe09ff2662698f17ca71fbc60070bd89e6138bd
CristianCamelo <cristiamauri08@gmail.com>
2025-02-06 22:18:06 -0500

    feat(dependencies): install dependencies

--- vetsforpets/tailwind.config.ts
+++ vetsforpets/tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "tailwindcss";
+import flowbite from "flowbite-react/tailwind";
 
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {
@@ -14,5 +16,8 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin(),
+
+  ],
 } satisfies Config;
