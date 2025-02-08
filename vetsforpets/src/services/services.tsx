5a40e19b77fb6fe82460755d82a38cd50759e504
Sebastian Sanjorge <sanjorgesebastian@gmail.com>
2025-02-07 21:51:38 -0300

    feat(loginForm.tsx): added login srevices

---
+++ vetsforpets/src/services/services.tsx
@@ -0,0 +1,29 @@
+import { ILoginResponse, IUserCredentials } from "./interfaces";
+
+const apiURL = process.env.NEXT_PUBLIC_API_URL;
+
+export async function loginUser(
+  userCredentials: IUserCredentials
+): Promise<ILoginResponse> {
+  try {
+    const response = await fetch(`${apiURL}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userCredentials),
+    });
+
+    if (!response.ok) {
+      throw new Error("Error al enviar formulario de inicio de sesión");
+    }
+
+    const data: ILoginResponse = await response.json();
+    return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+    throw new Error("Ocurrió un error desconocido");
+  }
+}
