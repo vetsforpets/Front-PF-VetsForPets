9ad6357d8de166598408e7fbdaba1504893a63f9
Maria Paz Rivas <pazroscianorivas@gmail.com>
2025-02-10 02:28:18 -0300

    cambios registro mascota

--- vetsforpets/src/app/page.tsx
+++ vetsforpets/src/app/page.tsx
@@ -1,9 +1,12 @@
+import PetForm from "../Components/PetForm/PetForm"
+
 export default function Home() {
   return (
     <>
       <div className="flex flex-col gap-3 py-10 items-center justify-center">
         <h1 className="customButton text-4xl font-bold">VETS FOR PETS</h1>
         <p>Inicio</p>
+        <PetForm />
       </div>
     </>
   );
