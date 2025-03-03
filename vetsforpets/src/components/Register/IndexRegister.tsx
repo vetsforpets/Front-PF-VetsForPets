91e8c69980d2c5d87066460c364d6146e5939077
CristianCamelo <cristiamauri08@gmail.com>
2025-03-03 12:03:25 -0500

    style(indexRegister): responsive pro y mejora en distribución

--- vetsforpets/src/components/Register/IndexRegister.tsx
+++ vetsforpets/src/components/Register/IndexRegister.tsx
@@ -31,14 +31,14 @@ import Link from 'next/link';
 
 const PetImageSection: React.FC = () => {
   return (
-    <div className="flex flex-col h-screen my-10 md:flex-row">
+    <div className="flex flex-col justify-center h-full mx-auto my-10 align-middle mt-14 md:flex-row">
       {/* Imagen izquierda */}
-      <Link href="/pet-owner-form">
-        <div className="relative w-full h-full overflow-hidden group">
+      <Link href="/pet-owner-form" className='relative flex w-auto h-full mx-4 overflow-hidden align-top md:justify-end group md:h-4/5 md:w-4/5 md:mx-0 md:ml-28 lg:max-w-md'>
+        <div className="">
           <img
             src="/woman-growing-plants-home-holding-cat 8.11.37 p. m..jpg"
             alt="Imagen 1"
-            className="object-cover w-full h-full transition-all duration-500 ease-out md:rounded-tl-3xl md:rounded-bl-3xl filter blur-none md:blur-sm saturate-60 group-hover:saturate-100 group-hover:blur-none"
+            className="object-cover w-full h-full transition-all duration-500 ease-out rounded-tl-3xl rounded-tr-3xl md:rounded-tr-none md:rounded-bl-3xl filter blur-none md:blur-sm saturate-60 group-hover:saturate-100 group-hover:blur-none"
           />
           <h1 className="absolute text-5xl font-bold text-white transform -translate-y-1/2 top-1/2 right-10 text-shadow">
             Dueño
@@ -48,14 +48,14 @@ const PetImageSection: React.FC = () => {
       </Link>
 
       {/* Imagen derecha */}
-      <Link href="/vet-form">
-        <div className="relative w-full h-full overflow-hidden group">
+      <Link href="/vet-form" className='relative flex justify-start w-auto h-full mx-4 overflow-hidden align-top group md:h-4/5 md:w-4/5 md:mx-0 md:mr-28 lg:max-w-md'>
+        <div className="">
           <img
             src="/veterinarian-taking-care-pet-dog.jpg 20-10-39-116.jpg"
             alt="Imagen 2"
-            className="object-cover w-full h-full transition-all duration-300 md:rounded-tr-3xl md:rounded-br-3xl filter md:blur-sm blur-none saturate-60 group-hover:saturate-100 group-hover:blur-none"
+            className="object-cover w-full h-full transition-all duration-500 ease-out rounded-bl-3xl md:rounded-tr-3xl rounded-br-3xl filter md:blur-sm blur-none saturate-60 group-hover:saturate-100 group-hover:blur-none"
           />
-          <h1 className="absolute text-5xl font-bold text-white transform -translate-y-1/2 top-1/2 left-10 text-shadow">
+          <h1 className="absolute text-5xl font-bold text-white transform -translate-y-1/2 top-1/2 left-4 md:left-4 lg:left-10 text-shadow">
             Veterinaria
           </h1>
           <div className="absolute inset-y-0 left-0 w-1 transition-all duration-300 bg-white shadow-lg opacity-0 group-hover:opacity-100"></div>
