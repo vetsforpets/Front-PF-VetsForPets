a5d1716cdc7b3dc6b575681bcb0735ecf924d858
ABengelsdorff <Angelica.bengelsdorff.5@gmail.com>
2025-03-05 19:28:53 -0300

    Renderizado del mapa en la seccion energencia

--- vetsforpets/src/app/emergencies/page.tsx
+++ vetsforpets/src/app/emergencies/page.tsx
@@ -1,22 +1,23 @@
 "use client";
-
-import { RequestEmergencyButton } from "@/components/EmergencyButton/EmergencyButton";
+import dynamic from "next/dynamic";
 import { VetEmergencies } from "@/components/VetEmergencies/VetEmergencies";
-// import { ChatComponent } from "@/components/Chat/ChatComponent";
-// import { useUserStore, useVetStore } from "@/store";
 import { useUserStore } from "@/store";
 import React from "react";
 
+// Importación dinámica para deshabilitar SSR en el componente del mapa
+const MapComponent = dynamic(() => import("../../components/Maps/Maps"), {
+  ssr: false,
+});
+
 export default function Emergencies() {
   const { userData } = useUserStore();
-  // const { vetId } = useVetStore();
 
   return (
     <>
       {userData?.role === "PETSHOP" ? (
         <VetEmergencies />
       ) : (
-        <RequestEmergencyButton petshopId="5925284a-cf5d-4fe4-89fc-88fa93e37cd4" />
+        <MapComponent />
       )}
     </>
   );
